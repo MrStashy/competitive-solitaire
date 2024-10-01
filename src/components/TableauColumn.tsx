1e410f991e4176db66fd1c55f9151c9bda809a4b
Will Baxter <bax.will@googlemail.com>
2024-10-01 12:48:26 +0100

    Tableau cards render

--- src/components/TableauColumn.tsx
+++ src/components/TableauColumn.tsx
@@ -8,6 +8,16 @@ type TableauColumnProps = {
 export default function TableauColumn({cards}: TableauColumnProps) {
 
     return(
-        <p className="border h-card-height w-card-width">Column</p>
+        <div className="flex flex-col relative h-[400px] w-[100px]">
+            {cards.map((card, index) => {
+                return    <img 
+                key={card.code} 
+                className="absolute top-${index * 10} left-0"
+                style={{ top: `${index * 20}px` }}
+                src={card.images.png} 
+                alt={`Card ${card.code}`} 
+            />
+            })}
+        </div>
     )
 }
