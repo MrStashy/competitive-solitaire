4569267662d2a36ee6d45067855316efcb0f7d05
Will Baxter <bax.will@googlemail.com>
2024-10-22 13:02:22 +0100

    Added leaderboard button

--- src/components/ControlModule.tsx
+++ src/components/ControlModule.tsx
@@ -1,32 +1,45 @@
-
-
 type ControlModuleProps = {
-    handleNewGameClick: () => void
-    handleRestartClick: () => void
-    dealt: boolean
-    loadingNewGame: boolean
-    endGame: () => void
-}
+  handleNewGameClick: () => void;
+  handleRestartClick: () => void;
+  dealt: boolean;
+  loadingNewGame: boolean;
+  endGame: () => void;
+};
 
-export default function ControlModule({ handleNewGameClick, handleRestartClick, dealt, loadingNewGame, endGame }: ControlModuleProps) {
+export default function ControlModule({
+  handleNewGameClick,
+  handleRestartClick,
+  dealt,
+  loadingNewGame,
+  endGame,
+}: ControlModuleProps) {
+  const style =
+    "text-2xl mb-10 bg-gradient-to-tr from-red-800 to-rose-500 border-2 p-4 rounded-lg text-white border-white shadow-md shadow-red-500/50 hover:shadow-none";
 
-    const style = "text-2xl mb-10 bg-gradient-to-tr from-red-800 to-rose-500 border-2 p-4 rounded-lg text-white border-white shadow-md shadow-red-500/50 hover:shadow-none"
-
-    if (dealt) {
-        return (
-            <div className="flex flex-col place-items-center">
-                <div className="flex flex-row gap-4">
-            <button className={style} onClick={handleRestartClick}>Restart</button>
-            <button className={style} onClick={() => endGame()}>Give Up</button>
-            </div>
-            </div>
-        )
-    }
-
-    if (!loadingNewGame)
+  if (dealt) {
     return (
-        <div className="flex flex-col place-items-center">
-       <button onClick={handleNewGameClick} className={style}>New Game</button>
-       </div>
-    )
+      <div className="flex flex-col place-items-center">
+        <div className="flex flex-row gap-4">
+          <button className={style} onClick={handleRestartClick}>
+            Restart
+          </button>
+          <button className={style} onClick={() => endGame()}>
+            Give Up
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  if (!loadingNewGame)
+    return (
+      <div className="flex flex-col place-items-center">
+        <div className="flex flex-row gap-4">
+          <button onClick={handleNewGameClick} className={style}>
+            New Game
+          </button>
+          <button className={style}>Leaderboard</button>
+        </div>
+      </div>
+    );
 }
